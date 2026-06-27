149ad1ba36e8c9867036808a00f0cd500aae8bf6
GREXLABS <mkmahabbat123x@gmail.com>
2026-06-27 10:06:16 +0530

    v1.2.1 rebrand: version.ts

--- packages/core/src/installation/version.ts
+++ packages/core/src/installation/version.ts
@@ -1,8 +1,8 @@
 declare global {
-  const OPENCODE_VERSION: string
-  const OPENCODE_CHANNEL: string
+  const GREXCODE_VERSION: string
+  const GREXCODE_CHANNEL: string
 }
 
-export const InstallationVersion = typeof OPENCODE_VERSION === "string" ? OPENCODE_VERSION : "local"
-export const InstallationChannel = typeof OPENCODE_CHANNEL === "string" ? OPENCODE_CHANNEL : "local"
+export const InstallationVersion = typeof GREXCODE_VERSION === "string" ? GREXCODE_VERSION : "local"
+export const InstallationChannel = typeof GREXCODE_CHANNEL === "string" ? GREXCODE_CHANNEL : "local"
 export const InstallationLocal = InstallationChannel === "local"
